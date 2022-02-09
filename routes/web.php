9800b8d748afc2fb673511a23c05b4e23c65a2f1
oxbow <oxbow@o2.pl>
2022-02-09 14:54:07 +0100

    admin controller and admin view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
