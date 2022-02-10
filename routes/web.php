ad28835e2dda7c161f62f3845b48339658b8bd1e
oxbow <oxbow@o2.pl>
2022-02-10 23:05:21 +0100

    Tag CRUD created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Livewire\TagIndex;
+use App\Http\Livewire\CastIndex;
+use App\Http\Livewire\GenreIndex;
+use App\Http\Livewire\MovieIndex;
+use App\Http\Livewire\SerieIndex;
+use App\Http\Livewire\SeasonIndex;
+use App\Http\Livewire\EpisodeIndex;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +25,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->get('/admin', [AdminController::class, 'index'])->name('admin.index');
+Route::middleware(['auth:sanctum', 'verified', 'role:admin'])->prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::get('movies', MovieIndex::class)->name('movies.index');
+    Route::get('series', SerieIndex::class)->name('series.index');
+    Route::get('series/{serie}/seasons', SeasonIndex::class)->name('seasons.index');
+    Route::get('series/{serie}/seasons/{season}/episodes', EpisodeIndex::class)->name('episodes.index');
+    Route::get('genres', GenreIndex::class)->name('genres.index');
+    Route::get('casts', CastIndex::class)->name('casts.index');
+    Route::get('tags', TagIndex::class)->name('tags.index');
+});
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
